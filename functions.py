9650f8352359409a2a4b304bcb62c1670adb4a1c
Elisa Afonso <elisa.afonso@insa-lyon.fr>
2026-01-11 18:26:34 +0100

    Ajout des deux cas pour l'analyse de sensibilité.

--- functions.py
+++ functions.py
@@ -100,7 +100,7 @@ def load_files(cell_mat_path, neighboring_cells_path):
         filtered_rows = df_filtered[df_filtered.iloc[:, 5] == 3.0]  # cancer cells 
 
         #Get only the cancer cells in conjonctive tissue, position between (1,2,3) = (x,y,z), below -115 in y axis
-        filtered_rows = df_filtered[df_filtered.iloc[:, 2] <= -115.0] #y <= -115    
+        filtered_rows = filtered_rows[filtered_rows.iloc[:, 2] <= -200.0] #y <= -200
         
         # IDs et volumes des cellules cancéreuses
         ids_cancer_cells = filtered_rows.iloc[:, 0].astype(int).tolist() #ids
@@ -261,6 +261,200 @@ def get_matrix_ids(files_by_timestep, root_path):
             print(f"file1 : {file1}")
     return result_mat
 
+#def computation_area_over_time(result_mat, dt): 
+#    """
+#    Input :
+#    result_mat : dictionnaire avec pour chaque pas de temps [clusters, total_cells, total_volume, timestep]
+#    Output :
+#    area_over_time : float, aire totale de la tumeur sur le temps
+#    """
+#    area_over_time = 0.0
+#    for key in result_mat.keys():
+#        if result_mat[key] != []:
+#            for cluster in result_mat[key][0]:  #clusters
+#                cluster_volume = sum(cluster.values())
+#                area_over_time += cluster_volume*dt #(volume tumeur / volume total) * dt
+ #   return area_over_time
+
+
+######## LISTER LES FICHIERS .mat #############
+def list_mat_files(folder_path):
+    path_folder_list = os.listdir(folder_path)
+
+    # Initialisation du dictionnaire de listes
+    files_by_timestep = defaultdict(list)
+
+    # Pattern pour récupérer l'info sur le timestep
+    pattern = re.compile(r"(?:output)0*([0-9]+)?")
+
+    for filename in path_folder_list:
+        match = pattern.match(filename)
+        if match:
+            timestep = match.group(1)
+            key = int(timestep) if timestep else 0
+        else:
+            continue  # ignorer les fichiers non conformes
+
+        if filename.endswith("_cells.mat"):
+            files_by_timestep[key].append(filename)
+
+    # Supprimer les entrées avec un nombre de fichiers incorrect, différents de 2 
+    keys_to_delete = []
+    for key in files_by_timestep.keys(): 
+        if len(files_by_timestep[key]) != 1: 
+            keys_to_delete.append(key)
+
+    for key in keys_to_delete: 
+        del files_by_timestep[key]
+    files_by_timestep = dict(files_by_timestep)
+    return files_by_timestep
+
+############## CALCUL DE LA METRIQUE POUR LA DESCENTE DE LA TUMEUR ###########
+def get_cancer_cell_position(cell_mat_path):
+    """
+    Input :
+    cell_mat_path : path to the .mat file with cell information
+    
+    Output :
+    position_y : y position of the cancer cell (None if no cancer cell)
+    ratio : temps de descente dans le tissu conjonctif / temps total de simulation
+    """
+    position_y = None
+    ratio = None #temps de descente dans le tissu conjonctif / temps total de simulation
+    
+    try: 
+        mat = scipy.io.loadmat(cell_mat_path)
+        cells = mat.get("cells")  
+        cells_T = cells.T #pour avoir les labels en colonne
+        df = pd.DataFrame(cells_T)
+        df_filtered = df.iloc[:, 0:6] #récupérer les 6 premières colonnes (id, (x,y,z), volume, type cellulaire)
+        filtered_rows = df_filtered[(df_filtered.iloc[:, 5] == 3.0) | (df_filtered.iloc[:, 5] == 8.0)]  # cancer cells (mesenchymal or not)
+        print(filtered_rows)
+
+        if len(filtered_rows) > 1 : 
+            raise ValueError("More than one cancer cell found.")
+        elif len(filtered_rows) == 0 :
+            ratio = 1
+            print("Too bad it's 0")
+        else : 
+            position_y = filtered_rows.iloc[0, 2] #y <= -200 : objective to be in connective tissue
+            print(position_y)
+
+
+    except Exception as e: 
+        print(f"Error loading file : {cell_mat_path} : {e}")
+        f = open( "log_file.log", "a")
+        f.write(f"\nError loading file : {cell_mat_path} : {e}")
+        f.close()
+    return ratio, position_y
+
+def compute_time_ratio(files_by_timestep, root_path, position_conj):
+    """
+    Input :
+    files_by_timestep : dict {timestep: [file1, file2]}
+    root_path : path to the root folder
+    Output :
+    ratio : temps de descente dans le tissu conjonctif / temps total de simulation
+    """
+    output_path = os.path.join(root_path, "PhysiCell/output")
+    timesteps = files_by_timestep.keys()
+    simulation_time = max(timesteps) 
+
+    result_mat = {} #initialisation du dictionnaire de résultats
+    for timestep in files_by_timestep.keys():
+        result_mat[timestep]= []
+        try:
+            file1 = os.path.join(output_path, files_by_timestep[timestep][0]) #cell_mat_path
+            ratio, position_y = get_cancer_cell_position(file1)  # Input : cell_mat_path 
+            if position_y is not None : 
+                if position_y <= position_conj : 
+                    time_in_conj_tissue = timestep
+                    print(f"Le cancer cell est dans le tissu conjonctif à l'étape {timestep}")
+                    ratio = time_in_conj_tissue / simulation_time
+                    print(simulation_time)
+                    print(f"Ratio : {ratio}")
+                    break
+            else : 
+                ratio = 1
+                break #no cancer cell found, so did not reach the connective tissue
+        except Exception as e:
+            f = open( "log_file.log", "a")
+            f.write(f"************************************** \n Erreur à l'étape {timestep} : {e} \n file1 : {file1} \n **************************************")
+            f.close()
+            print(f"Erreur à l'étape {timestep} : {e}")
+            print(f"file1 : {file1}")
+    return ratio #métrique finale pour 1 simulation ! 
+
+############## CALCUL DE LA METRIQUE POUR LA PERSISTANCE DE LA TUMEUR ###########
+def get_cancer_volume_per_timestep(cell_mat_path, pos_conj):
+    """
+    Input :
+    cell_mat_path : path to the .mat file with cell information
+    pos_conj : position y threshold for conjonctive tissue
+
+    Output :
+    """
+    filtered_rows = None
+    ids_volume_dict = None
+    
+    try: 
+        # Charger le fichier .mat avec le descriptif (e.g. les labels) de chaque agent 
+        mat = scipy.io.loadmat(cell_mat_path)
+        cells = mat.get("cells")  
+        cells_T = cells.T #pour avoir les labels en colonne
+        df = pd.DataFrame(cells_T)
+        df_filtered = df.iloc[:, 0:6] #récupérer les 6 premières colonnes (id, (x,y,z), volume, type cellulaire)
+        filtered_rows = df_filtered[(df_filtered.iloc[:, 5] == 3.0) | (df_filtered.iloc[:, 5] == 8.0)] # cancer cells (mesenchymal or not)
+
+        #Get only the cancer cells in conjonctive tissue, position between (1,2,3) = (x,y,z), below -115 in y axis
+        filtered_rows = filtered_rows[filtered_rows.iloc[:, 2] <= pos_conj] #y <= -200 (pos_conj)
+        # IDs et volumes des cellules cancéreuses
+        ids_cancer_cells = filtered_rows.iloc[:, 0].astype(int).tolist() #ids  
+        print(f"ids_cancer_cells : {ids_cancer_cells}")
+        ids_cancer_cells_volume = filtered_rows.iloc[:, 4].astype(float).tolist() #volumes
+        ids_volume_dict = {ids_cancer_cells[i]: ids_cancer_cells_volume[i] for i in range(len(ids_cancer_cells))} 
+
+    except Exception as e: 
+        print(f"Error loading file : {cell_mat_path} : {e}")
+        f = open( "log_file.log", "a")
+        f.write(f"\nError loading file : {cell_mat_path}: {e}")
+        f.close()
+        #add a warning log here
+    return ids_volume_dict
+
+def get_result_mat_persistance(files_by_timestep, root_path, pos_conj):
+    """
+    Input :
+    files_by_timestep : dict {timestep: [file1, file2]}
+    root_path : path to the root folder
+    pos_conj : position y threshold for conjonctive tissue
+
+    Output :
+    result_mat : dict {timestep: [clusters, total_cells, total_volume, timestep]}
+    """
+    output_path = os.path.join(root_path, "PhysiCell/output/")
+
+    result_mat = {} #initialisation du dictionnaire de résultats
+    for timestep in files_by_timestep.keys():
+        result_mat[timestep]= []
+        try:
+            file1 = os.path.join(output_path, files_by_timestep[timestep][0]) #cell_mat_path
+            result_array = get_cancer_volume_per_timestep(file1, pos_conj)  # Input : cell_mat_path, neighboring_cells_path / doit retourner une liste ou array de taille 3 de la forme [liste avec des sets avec les ids de chaque cellule dans chaque cluster, nb d'agents, volume total]
+            if result_array is not None :
+                result_mat[timestep] = result_array
+            else : 
+                f = open( "log_file.log", "a")
+                f.write(f"Problème à l'étape {timestep} : résultat None")
+                f.close()
+                print(f"Problème à l'étape {timestep} : résultat None")
+        except Exception as e:
+            f = open( "log_file.log", "a")
+            f.write(f"************************************** \n Erreur à l'étape {timestep} : {e} \n file1 : {file1} \n **************************************")
+            f.close()
+            print(f"Erreur à l'étape {timestep} : {e}")
+            print(f"file1 : {file1}")
+    return result_mat
+
 def computation_area_over_time(result_mat, dt): 
     """
     Input :
@@ -270,8 +464,16 @@ def computation_area_over_time(result_mat, dt):
     """
     area_over_time = 0.0
     for key in result_mat.keys():
-        if result_mat[key] != []:
-            for cluster in result_mat[key][0]:  #clusters
-                cluster_volume = sum(cluster.values())
-                area_over_time += cluster_volume*dt #(volume tumeur / volume total) * dt
+        if result_mat[key] != {}:
+            volume = sum(result_mat[key].values())
+            area_over_time += volume*dt #(volume tumeur / volume total) * dt
     return area_over_time
+
+
+if __name__ == "__main__":
+    folder_path = "C://Users/elisa/Physicell/output"
+    root_path = "C://Users/elisa/"
+
+    #files_by_timestep = list_path_folder(folder_path)
+    #result_mat = get_matrix_ids(files_by_timestep, root_path)
+    #print(result_mat)
--- test_analyse_sensibilite.py
+++ test_analyse_sensibilite.py
@@ -1,8 +1,10 @@
+# A PENSER A MODIFIER : La position du tissu conjonctif dans les conditions initiales et les ids des cellules s'ils ont été modifiés. 
+
 from SALib.analyze.sobol import analyze
 from SALib.sample.sobol import sample
 import numpy as np
 import xml.etree.ElementTree as ET
-from functions import explore_tree, dict_to_xml, list_path_folder, get_matrix_ids, computation_area_over_time
+from functions import explore_tree, dict_to_xml, list_path_folder, get_matrix_ids, computation_area_over_time, list_mat_files, compute_time_ratio, get_result_mat_persistance
 import os 
 import subprocess
 from collections import defaultdict
@@ -14,33 +16,81 @@ import shutil
 
 def define_set_param(): 
     print("Defining parameter space and sampling...")
-    problem = {
-        'num_vars': 2,
-        'names': ['life_cycle_cancer_cell', 'transformation_rate_epi_basal_cancer'],
-        'bounds': [[720, 2880], [0.0000001, 0.000001]]
-    }
+
+    if analyse_sensibilite == "descent_time" : 
+        problem = {
+            'num_vars': 5,
+            'names': ['attachment_rate', 'cancer_motility_speed', 'transformation_rate_mes', 'cell_adhesion_affinity_LM_EP', 'cell_adhesion_affinity_LB_conj'],
+            'bounds': [[720, 2880], [0.0000001, 0.000001]] #TO CHANGE
+        }
+    elif analyse_sensibilite == "tumor_persistance" :
+        problem = {
+            'num_vars': 2,
+            'names': ['motility_speed_t_cell', 'division_duration_cancer'],
+            'bounds': [[720, 2880], [0.0000001, 0.000001]] #TO CHANGE
+        }
 
     param_values = sample(problem, 2) #Génère N*(2+D) jeux de paramètres avec D le nombre de paramètres et N un multiple de 2 fourni en argument
     return problem, param_values
 
-def define_settings(life_cycle_cancer_cell, transformation_rate_epi_basal_cancer): 
+def define_settings(parameters_to_change): 
     print("Defining XML settings for the simulation...")
     tree = ET.parse(os.path.join(root_path, "PhysiCell/config/PhysiCell_settings.xml"))
     root = tree.getroot()
     dict_xml = {root.tag: explore_tree(root)}
+
     #changement des paramètres 
-    dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][0]["phenotype"]["cell_transformations"]["transformation_rates"]["transformation_rate"][3]["text_explanation"] = transformation_rate_epi_basal_cancer
-    dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][3]["phenotype"]["cycle"]["phase_durations"]["duration"]["text_explanation"] = life_cycle_cancer_cell
-    dict_xml["PhysiCell_settings"]["overall"]["max_time"]["text_explanation"] = 240 #modification du temps de simulation
+
+    ################## DESCENT TIME ####################
+    if analyse_sensibilite == "descent_time" :
+        # Attachment rate 
+        attachment_rate = parameters_to_change[0]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][5]["phenotype"]["mechanics"]["attachment_rate"]["text_explanation"] = attachment_rate
+
+        #Motility speed cancer cells
+        cancer_motility_speed = parameters_to_change[1]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][3]["phenotype"]["motility"]["speed"]["text_explanation"] = cancer_motility_speed
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][8]["phenotype"]["motility"]["speed"]["text_explanation"] = cancer_motility_speed
+
+        # Mes transition 
+        transformation_rate_mes = parameters_to_change[2]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][3]["phenotype"]["cell_transformations"]["transformation_rates"]["transformation_rate"][8]["text_explanation"] = transformation_rate_mes
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][8]["phenotype"]["cell_transformations"]["transformation_rates"]["transformation_rate"][3]["text_explanation"] = transformation_rate_mes
+
+        #Adhésion lame basale/épi basal
+        cell_adhesion_affinity_LM_EP = parameters_to_change[3]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][5]["phenotype"]["mechanics"]["cell_adhesion_affinities"]["cell_adhesion_affinity"][0]["text_explanation"] = cell_adhesion_affinity_LM_EP
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][0]["phenotype"]["mechanics"]["cell_adhesion_affinities"]["cell_adhesion_affinity"][5]["text_explanation"] = cell_adhesion_affinity_LM_EP
+
+        #Adhésion MB/Conj 
+        cell_adhesion_affinity_LB_conj = parameters_to_change[4]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][5]["phenotype"]["mechanics"]["cell_adhesion_affinities"]["cell_adhesion_affinity"][6]["text_explanation"] = cell_adhesion_affinity_LB_conj
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][6]["phenotype"]["mechanics"]["cell_adhesion_affinities"]["cell_adhesion_affinity"][5]["text_explanation"] = cell_adhesion_affinity_LB_conj
+
+    ################## TUMOR PERSISTANCE ####################
+    elif analyse_sensibilite == "tumor_persistance" :
+        # Vitesse motilité T-Cell 
+        motility_speed_t_cell = parameters_to_change[0]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][4]["phenotype"]["motility"]["speed"]["text_explanation"] = motility_speed_t_cell
+
+        # Division cellules cancéreuses (mes et non mes)
+        division_duration_cancer = parameters_to_change[0]
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][3]["phenotype"]["cycle"]["phase_durations"]["duration"]["text_explanation"] = division_duration_cancer
+        dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][8]["phenotype"]["cycle"]["phase_durations"]["duration"]["text_explanation"] = division_duration_cancer
+
+    #dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][0]["phenotype"]["cell_transformations"]["transformation_rates"]["transformation_rate"][3]["text_explanation"] = transformation_rate_epi_basal_cancer
+    #dict_xml["PhysiCell_settings"]["cell_definitions"]["cell_definition"][3]["phenotype"]["cycle"]["phase_durations"]["duration"]["text_explanation"] = life_cycle_cancer_cell
+    #dict_xml["PhysiCell_settings"]["overall"]["max_time"]["text_explanation"] = 240 #modification du temps de simulation
     return dict_xml
 
 def get_physicell_output(param_values): 
     output_storage = np.zeros(param_values.shape[0]) 
     for i in range (len(param_values)): 
         print(f"Running simulation {i+1} with parameters: life_cycle_cancer_cell={param_values[i][0]}, transformation_rate_epi_basal_cancer={param_values[i][1]}")
-        life_cycle_cancer_cell = param_values[i][0]
-        transformation_rate_epi_basal_cancer = param_values[i][1]
-        dict_xml = define_settings(life_cycle_cancer_cell, transformation_rate_epi_basal_cancer)
+        #life_cycle_cancer_cell = param_values[i][0]
+        #transformation_rate_epi_basal_cancer = param_values[i][1]
+        parameters_to_change = param_values[i]
+        dict_xml = define_settings(parameters_to_change)
 
         xml_element = dict_to_xml(dict_xml)
         tree = ET.ElementTree(xml_element)
@@ -69,12 +119,21 @@ def get_physicell_output(param_values):
         print(process1.stderr)
 
         #get output from output folder 
-        output_path = os.path.join(root_path, "PhysiCell/output")  
-        files_by_timestep = list_path_folder(output_path)
-        result_mat = get_matrix_ids(files_by_timestep, root_path)
-        dt = int(dict_xml["PhysiCell_settings"]["save"]["full_data"]["interval"]["text_explanation"])/60.0  # conversion en heures
-        area_over_time = computation_area_over_time(result_mat, dt)
-        output_storage[i] = area_over_time
+        output_path = os.path.join(root_path, "PhysiCell/output") 
+
+        if analyse_sensibilite == "tumor_persistance" :  
+            pos_conj = 0 # A MODIFIER 
+            files_by_timestep = list_mat_files(output_path)
+            result_mat = get_result_mat_persistance(files_by_timestep, root_path, pos_conj)
+            dt = int(dict_xml["PhysiCell_settings"]["save"]["full_data"]["interval"]["text_explanation"])/60.0  # conversion en heures
+            metric = computation_area_over_time(result_mat, dt)
+        
+        elif analyse_sensibilite == "descent_time" : 
+            files_by_timestep = list_mat_files(output_path)
+            ratio = compute_time_ratio(files_by_timestep, root_path, position_conj = -200)  
+            metric = ratio
+
+        output_storage[i] = metric #computation metric
 
         #stocker la vidéo 
         process2 = subprocess.run(
@@ -117,10 +176,11 @@ def analyze_sobol():
     return Si
 
 if __name__ == "__main__":
+    analyse_sensibilite = "tumor_persistance"
     root_path = "C://Users/elisa/" # !! A changer
     #get le xml et le copier dans config/ 
     #ouvrir interface graphique Tkinter
     #créer un fichier log
+    
     Si = analyze_sobol()
     print(f"Sobol Indices: {Si}")
-
